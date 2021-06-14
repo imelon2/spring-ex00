c246b3f3be315f370aad5420e9899b2024351a1c
imelon2 <imelon279@naver.com>
2021-06-14 16:26:57 +0900

    first

---
+++ src/main/java/lombok/test/Book.java
@@ -0,0 +1,16 @@
+package lombok.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Book {
+	
+	
+	
+	private String title;
+	private double price;
+}
---
+++ src/main/java/lombok/test/Desk.java
@@ -0,0 +1,7 @@
+package lombok.test;
+
+public class Desk {
+	public final double price;
+	private final String model;
+	
+}
