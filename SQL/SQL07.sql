41177bf5203dc531f80e72fc24ac80884281a093
imelon2 <imelon279@naver.com>
2021-07-05 17:47:18 +0900

    7.5

---
+++ SQL/SQL07.sql
@@ -0,0 +1,16 @@
+Use spr1;
+DESC tbl_board_file;
+
+CREATE TABLE tbl_board_file (
+	id int PRIMARY KEY AUTO_INCREMENT,
+	bno INT REFERENCES tbl_board(bno),
+	fileName VARCHAR(200) not NULL
+    );
+
+
+SELECT * FROM tbl_board_file;
+
+DELETE FROM tbl_board_file;
+
+ALTER TABLE tbl_board_file
+add FOREIGN KEY (bno) REFERENCES tbl_board(bno);
