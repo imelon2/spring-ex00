810eeb73ff80060e92a9b4db0864d69754eb74bb
imelon2 <imelon279@naver.com>
2021-07-16 17:29:05 +0900

    7016

--- SQL/SQL09.sql
+++ SQL/SQL09.sql
@@ -26,3 +26,34 @@ SET writer = 'member';
 UPDATE tbl_reply
 SET replyer = 'member';
 
+
+
+CREATE table empuser(
+    empuserid VARCHAR(50) PRIMARY KEY,
+    userpw VARCHAR(500) not null,
+    userName VARCHAR(50) not null,
+    userAddress VARCHAR(100) not null,
+    address VARCHAR(100) not null,
+    birthDate DATE DEFAULT now(),
+    userPhonenum int(50) not null,
+    userEmail VARCHAR(50) not null,
+    userSex int(5) not null,
+    userNickname VARCHAR(50) not null,
+    
+    lat DECIMAL(12,7) not null,
+    lag DECIMAL(12,7) not null
+);
+
+
+CREATE table user_auth(
+   ano int PRIMARY KEY AUTO_INCREMENT,
+    userid VARCHAR(50) not null,
+    auth VARCHAR(255) not null,
+   FOREIGN KEY (userid) REFERENCES user(userid),
+   FOREIGN KEY (userid) REFERENCES b2buser(b2buserid),
+   FOREIGN KEY (userid) REFERENCES empuser(empuserid)
+);
+
+drop TABLE user_auth;
+
+
