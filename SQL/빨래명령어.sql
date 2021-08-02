0640799586242318b818e6f53c61acb5b38c0b38
imelon2 <imelon279@naver.com>
2021-08-02 20:08:02 +0900

    8.2

--- SQL/빨래명령어.sql
+++ SQL/빨래명령어.sql
@@ -4,14 +4,19 @@ DESC user;
 DESC user_auth;
 DESC subscriber;
 DESC b2bIntroduce;
-DELETE FROM b2bIntroduce;
+
 
 SELECT * FROM user_revivew;
 
 SELECT * FROM user_auth;
 SELECT * FROM b2bIntroduce;
 SELECT * FROM storeinfo;
+SELECT * FROM subscriber;
 
+
+SELECT s.subsId
+			FROM user AS u JOIN subscriber AS s ON u.userid = s.subsId
+		WHERE subsId = "user";
 
                         
 SELECT
@@ -24,3 +29,7 @@ SELECT
 	b.fileName fileName
 FROM storeinfo AS s  LEFT JOIN b2bIntroduce AS b ON b.id = 5
 WHERE s.id = 5;
+
+SELECT  storename, storeaddress, storePhonenum
+FROM storeinfo
+WHERE id = 5;
